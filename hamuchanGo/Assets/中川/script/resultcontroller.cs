436740201f1ab3871beecee5f1d7ea63d66ff224
Kento nakagawa <ttbnars@gmail.com>
2018-08-17 16:19:24 +0900

    タイトルとリザルトの画面を作成しました。

---
+++ hamuchanGo/Assets/中川/script/resultcontroller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resultcontroller : MonoBehaviour
+{
+
+    public void OnButton()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+}
