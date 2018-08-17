da24ecd2c5dd933b4448d7ac39f5daf05b417ddf
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2018-08-17 15:06:55 +0900

    タイマーと持ち種を表示するスクリプトを作成

---
+++ hamuchanGo/Assets/Nakashima/SeedsStack.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Naka
+{
+    public class SeedsStack : MonoBehaviour
+    {
+        [SerializeField, Range(1, 30), Tooltip("種の最大数を指定")]
+        int seedMax = 10;
+        [SerializeField,Tooltip("見た感じの種の感覚")]
+        float seedSpriteInterval;
+        [SerializeField]
+        int seedNum;//現在表示している数
+        [SerializeField, Tooltip("この下の階層の種オブジェクトを指定")]
+        Image seed;
+        [SerializeField]
+        Sprite filledSeedSprite;
+        [SerializeField]
+        Sprite emptySeedSprite;
+
+        Image[] uiImages;
+        void Start()
+        {
+            uiImages = new Image[seedMax];
+            uiImages[0] = seed;
+            Vector2 instPos = seed.transform.localPosition;
+            for(int i = 1;i < seedMax; i++){
+                instPos += Vector2.right * seedSpriteInterval;
+                var instSeed = Instantiate(seed, transform).GetComponent<Image>();
+                instSeed.transform.localPosition = instPos;
+                uiImages[i]=instSeed;
+            }
+            for (int i = 0; i < seedNum; i++)
+            {
+                uiImages[i].sprite = filledSeedSprite;
+            }
+        }
+
+        public void PushSeed(int num = 1)
+        {
+            seedNum += num;
+            UpdateSeedSprite();
+        }
+        public void PopSeed(int num = 1)
+        {
+            seedNum -= num;
+            UpdateSeedSprite();
+        }
+
+        /// <summary>
+        /// 種画像の更新
+        /// </summary>
+        void UpdateSeedSprite()
+        {
+            for (int i = 0; i < uiImages.Length; i++)
+            {
+                if (i + 1 <= seedNum)
+                {
+                    uiImages[i].sprite = filledSeedSprite;
+                }
+                else
+                {
+                    uiImages[i].sprite = emptySeedSprite;
+                }
+            }
+        }
+    }
+}
--- hamuchanGo/Assets/Nakashima/Sprout.cs
+++ hamuchanGo/Assets/Nakashima/Sprout.cs
@@ -2,42 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sprout : MonoBehaviour {
+namespace Naka
+{
+    public class Sprout : MonoBehaviour
+    {
 
-    [SerializeField, Tooltip("芽が伸びる時間")]
-    float sproutTime;
-    [SerializeField, Tooltip("芽が伸び切ってから花が咲くまでの時間")]
-    float bloomTime;
-    [SerializeField, Tooltip("芽の足場の高さ")]
-    float sproutHight;
-    [SerializeField, Tooltip("芽から花までの高さ")]
-    float flowerHight;
-    [SerializeField,Tooltip("一個下の階層のLeefをアタッチする")]
-    Transform childLeef;//足場の部分
-    [SerializeField]
-    GameObject sunFlower;
+        [SerializeField, Tooltip("芽が伸びる時間")]
+        float sproutTime;
+        [SerializeField, Tooltip("芽が伸び切ってから花が咲くまでの時間")]
+        float bloomTime;
+        [SerializeField, Tooltip("芽の足場の高さ")]
+        float sproutHight;
+        [SerializeField, Tooltip("芽から花までの高さ")]
+        float flowerHight;
+        [SerializeField, Tooltip("一個下の階層のLeefをアタッチする")]
+        Transform childLeef;//足場の部分
+        [SerializeField]
+        GameObject sunFlower;
 
-    float timer = 0;
-    float firstLeefLocalY;
-    void Start () {
-        firstLeefLocalY = childLeef.localPosition.y;
-    }
-	
-	void Update () {
-        timer += Time.deltaTime;
-        if (timer <= sproutTime) {//発芽するまで
-            float growRate = timer / sproutTime;
-            var localPos = childLeef.localPosition;
-            localPos.y = firstLeefLocalY + growRate * sproutHight;
-            childLeef.localPosition = localPos;
-        }else if (timer<= sproutTime + bloomTime)//発芽してから咲くまで
+        float timer = 0;
+        float firstLeefLocalY;
+        void Start()
         {
-            
-        }else //咲くとき
-        {
-            var flower = Instantiate(sunFlower, transform);
-            var localPos = Vector2.up * (sproutHight + flowerHight);
-            flower.transform.localPosition = localPos;
+            firstLeefLocalY = childLeef.localPosition.y;
         }
-	}
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer <= sproutTime)
+            {//発芽するまで
+                float growRate = timer / sproutTime;
+                var localPos = childLeef.localPosition;
+                localPos.y = firstLeefLocalY + growRate * sproutHight;
+                childLeef.localPosition = localPos;
+            }
+            else if (timer <= sproutTime + bloomTime)//発芽してから咲くまで
+            {
+
+            }
+            else //咲くとき
+            {
+                var flower = Instantiate(sunFlower, transform);
+                var localPos = Vector2.up * (sproutHight + flowerHight);
+                flower.transform.localPosition = localPos;
+            }
+        }
+    }
 }
