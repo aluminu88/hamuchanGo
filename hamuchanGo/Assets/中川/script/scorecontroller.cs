1f3c1f4957e72be532d8e46970e18cdf03084da2
Kento nakagawa <ttbnars@gmail.com>
2018-08-20 12:10:34 +0900

    デザインの改良

---
+++ hamuchanGo/Assets/中川/script/scorecontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Neno.Scripts;
+using UnityEngine.UI;
+
+public class scorecontroller : MonoBehaviour {
+
+    [SerializeField] private Text resultScore;
+
+    void Start() {
+
+
+        //GameRuleManager.Instance.TotalScore
+        resultScore.text = GameRuleManager.Instance.TotalScore.ToString();
+
+    }
