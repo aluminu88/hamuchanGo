e510569860556e6c52623b45cae616205fbe6c72
nenoNaninu <neno@neno-garden.com>
2018-08-21 13:11:28 +0900

    ios版完成。ステージとかちゃんとできたらそこに取り込む

--- hamuchanGo/Assets/neno/script/Player.cs
+++ hamuchanGo/Assets/neno/script/Player.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using UnityStandardAssets.CrossPlatformInput;
 
 namespace Neno.Scripts
 {
@@ -99,7 +99,7 @@ namespace Neno.Scripts
             this.SheedNum = PlayerStatusModel.Instance.SeedNum;
             this.Hp = PlayerStatusModel.Instance.PlayerHp;
             this.maxSeedNum = PlayerStatusModel.Instance.MaxSeedNum;
-            
+
             //SceneManager.sceneLoaded += (scene, mode) =>
             //{
             //    this.SheedNum = PlayerStatusModel.Instance.SeedNum;
@@ -113,9 +113,67 @@ namespace Neno.Scripts
                 PlayerStatusModel.Instance.PlayerHp = this.Hp;
             };
         }
-        
+
         void FixedUpdate()
         {
+#if UNITY_IOS
+            if (!this.isPlay)
+            {
+                return;
+            }
+
+            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+            //horizontal = horizontal < 0 ? -0.1f : 0.1f;
+            //if(){
+            //   
+            //}
+            horizontal /= 10;
+            Debug.Log(horizontal);
+            float jumpVelocityHorizontal = horizontal * 5;
+
+
+            var jumpVelocity = new Vector3(jumpVelocityHorizontal, JumpPower, 0);
+
+            Vector3 addPos = new Vector3(horizontal, 0f, 0f);
+
+            if(isGround){
+                this.playeRigidbody.transform.position = this.playeRigidbody.transform.position + new Vector3(horizontal, 0, 0);
+                this.playeRigidbody.transform.right = new Vector3(horizontal, 0, 0);
+
+            }else{
+                if(0 < horizontal){
+                    if (playeRigidbody.velocity.x < 10)
+                    {
+                        this.playeRigidbody.velocity += new Vector2(0.2f, 0);
+                    }
+                }else if(0 > horizontal){
+                    if (-10 < playeRigidbody.velocity.x)
+                    {
+                        this.playeRigidbody.velocity += new Vector2(-0.2f, 0);
+                    }
+                }
+            }
+
+            if(CrossPlatformInputManager.GetButtonDown("Jump")){
+                if (isGround)
+                {
+                    this.isGround = !this.isGround;
+
+                    this.playeRigidbody.AddForce(jumpVelocity, ForceMode2D.Impulse);
+                }
+            }
+
+
+            animator.SetBool("jump", !isGround);
+
+            //球を発射
+            if (CrossPlatformInputManager.GetButtonDown("Attack"))
+            {
+                ShootSeed();
+            }
+
+#else
+
             if (!this.isPlay)
             {
                 return;
@@ -173,6 +231,7 @@ namespace Neno.Scripts
             {
                 ShootSeed();
             }
+#endif
         }
 
         bool HasSheeds()
